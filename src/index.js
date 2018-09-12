be080260b476f884171c9a5c7046e5a928a1533c
Artem Zekov <mrstalon@MacBook-Air-Artem.local>
2018-09-12 15:21:45 +0300

    task is ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const slicedStr = str.split('');
+  const stack = new Stack();
+  slicedStr.forEach((char) => {
+    isCharDependsToOpenOrClose(char, bracketsConfig);
+  });
+
+  if (stack.getLength() > 0) {
+    return false;
+  }
+  return true;
+
+
+
+
+
+  function isCharDependsToOpenOrClose(char, bracketsConfig) {
+    bracketsConfig.forEach((config) => {
+      if (config[0] === config[1]) {
+        // handling special cases
+        if (char === config[0]) {
+          if (stack.getTopElement() === config[0]) {
+            // if closing bracket has closing pair
+            stack.pop();
+            return;
+          } else {
+            stack.push(char);
+            return;
+          }
+        }
+      }
+      if (char === config[0]) {
+        // if char depends to open brackets
+        stack.push(char);
+        return;
+      } else if (char === config[1]) {
+        // if char depends to close brackets
+        if (stack.getTopElement() === config[0]) {
+          // if closing bracket has closing pair
+          stack.pop();
+          return;
+        } else {
+          stack.push(char);
+          return;
+        }
+      }
+    });
+  }
+}
+
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(element) {
+    this.stack.push(element);
+  }
+
+  pop(element) {
+    this.stack.pop();
+  }
+
+  getTopElement() {
+    if (this.stack.length === 0) {
+      return undefined;
+    }
+    return this.stack[this.stack.length - 1];
+  }
+
+  getLength() {
+    return this.stack.length;
+  }
+
+  printStack() {
+    console.log(this.stack);
+  }
 }
